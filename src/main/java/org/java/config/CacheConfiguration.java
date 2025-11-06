fbd2c28847612f04cb05d2915edcefe2d656bb0f
thiago santos <thiagocroza472@gmail.com>
2025-11-06 20:35:23 -0300

    feat: add cache configuration with Spring's SimpleCacheManager
    - Introduced `CacheConfiguration` for enabling caching with Spring.
    - Configured `SimpleCacheManager` with a cache for reports.

---
+++ src/main/java/org/java/config/CacheConfiguration.java
@@ -0,0 +1,23 @@
+package org.java.config;
+
+import java.util.List;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+  @Bean
+  public CacheManager cacheManager() {
+    SimpleCacheManager cacheManager = new SimpleCacheManager();
+    cacheManager.setCaches(List.of(
+        new ConcurrentMapCache("reports")
+    ));
+    return cacheManager;
+  }
+}
